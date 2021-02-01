8588ff5ecbb90f95fee275798623b74ff9904564
Paul <72827473+pnguyen6910@users.noreply.github.com>
2021-01-31 21:27:38 -0600

    updated port variable

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require("fs")
 const db = require('./db/db.json')
 const uniqId = require("uniqid")
 const app = express()
-const PORT = process.env.PORT || 3000
+var PORT = process.env.PORT || 3000
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
