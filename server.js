417aec4f9bf49c4558b36c1b467af82e5e800351
Paul <72827473+pnguyen6910@users.noreply.github.com>
2021-01-31 19:29:19 -0600

    added page for notes html

--- server.js
+++ server.js
@@ -10,5 +10,12 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(express.static("public"))
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + 'public/index.html'))
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/notes.html'))
+})
 
 
