edb14b97d696cea62f1d16375e8b497915381ac3
Paul <72827473+pnguyen6910@users.noreply.github.com>
2021-01-31 19:27:16 -0600

    added home page for index html

--- server.js
+++ server.js
@@ -4,5 +4,11 @@ const fs = require("fs")
 const db = require(./db/db.json)
 const uniqid = require("uniqid")
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.static("public"))
+
+
+
